3f7c64cae3a75588e533d2f7476aec3fdc759bf4
Beni Peralta <ramosperaltabenigno@gmail.com>
2024-11-16 10:38:15 -0600

    Hola es el segundo Commit

--- bbva.java
+++ bbva.java
@@ -1,5 +1,22 @@
+
+class Persona {
+    String nombre;
+    String sexo;
+    int edad;
+
+    Persona(String nombre, String sexo, int edad){
+        this.nombre= nombre;
+        this.sexo= sexo;
+        this.edad= edad;
+
+    }
+    void hablar(){
+        System.out.println("Hola soy una persona");
+    }
+}
+
 class Programa {
     public static void main(String []args){
-        System.out.printls("Hello World")
+        System.out.println("Hello World")
     }
 }
