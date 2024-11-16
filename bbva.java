c98dfdb8331349c8d94fde9e903c6b7294dfc4d5
Beni Peralta <ramosperaltabenigno@gmail.com>
2024-11-16 12:46:37 -0600

    Hola es el tercer Commit

--- bbva.java
+++ bbva.java
@@ -14,9 +14,49 @@ class Persona {
         System.out.println("Hola soy una persona");
     }
 }
+class Cliente extends Persona{
+    int numero;
+    Cliente(String nombre,String sexo, int edad, int numero){
+        super(nombre,sexo, edad);
+this.numero= numero;
 
-class Programa {
-    public static void main(String []args){
-        System.out.println("Hello World")
+    }
+}
+class Banco{
+    String nombre;
+    Cliente  clientes[];
+
+    Banco(int cantidadClientes,String nombre){
+        clientes = new Cliente[cantidadClientes];
+        this.nombre=nombre;
+    }
+    Cliente crearCliente(String nombre, String sexo, int edad, int numero){
+        System.out.println("Creando cliente.....");
+        Cliente cliente=new Cliente(numero, sexo, edad, nombre);
+
+        for(int i=0; i < clientes.length;i++){
+            if(clientes[i]==null){
+                clientes[i]=cliente;
+                System.out.println("Cliente creado exitosamente");
+                break;
+            }
+        }
+        return cliente;
+    }
+    void listarClientes(){
+        for(int i =0; i < clientes.length;i++){
+            if(clientes[i] !=null){
+                Cliente cliente = clientes[i];
+                System.out.println("Cliente "+ cliente.nombre+"No: " + cliente.numero);
+                
+            }
+        }
+    }
+}
+
+class Programa{
+    public static void main(String[] args){
+       System.out.println("Welcome to the bank"); 
+
     }
 }
